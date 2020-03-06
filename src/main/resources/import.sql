ae8dbd4596fa8e502afd4e548402c7b825889f03
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2020-03-06 08:38:26 +0100

    init

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO FOO(id, name, created_at, updated_at) VALUES(1, 'Alma', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO FOO(id, name, created_at, updated_at) VALUES(2, 'Korte', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO FOO(id, name, created_at, updated_at) VALUES(3, 'Barack', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
